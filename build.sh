891dea68f756a4e4dfcc5b0027ee6c255b0a46ea
El Mostafa Idrassi <mostafa.idrassi@tutanota.com>
2023-01-27 21:11:40 +0100

    Added -Wno-deprecated-declarations to avoid showing deprecations

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ testengine.cpp -g3 -O0 -ggdb -fPIC -rdynamic -shared -ldl -lrt -lssl -lcrypto -Wl,-soname,libtestengine.so -o libtestengine.so
+g++ testengine.cpp -Wno-deprecated-declarations -g3 -O0 -ggdb -fPIC -rdynamic -shared -ldl -lrt -lssl -lcrypto -Wl,-soname,libtestengine.so -o libtestengine.so
